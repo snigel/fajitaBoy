d33f448d2abc0499543e62158f360407509e8e2b
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2009-03-17 02:29:12 +0000

    glömde lägga till denna, debuggern skiter sig utan denna :/

---
+++ src/fajitaboy/InterruptedStepException.java
@@ -0,0 +1,20 @@
+package fajitaboy;
+
+public class InterruptedStepException extends Exception {
+    private String msg;
+    private int cycles;
+    
+    
+    public InterruptedStepException(final String msg, final int cycles) {
+        this.msg = msg;
+        this.cycles = cycles;
+    }
+
+    public String toString() {
+        return msg;
+    }
+    
+    public int getCycles() {
+        return cycles;
+    }
+}
