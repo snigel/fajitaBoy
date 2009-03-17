1263c50ae3829546fed2f4a41b00cbb2b233578c
Adam Hulin <adam.hulin@gmail.com>
2009-03-17 23:23:47 +0000

    Skapade bitmaskConstants för att slippa magic numbers

---
+++ src/fajitaboy/constants/BitmaskConstants.java
@@ -0,0 +1,9 @@
+package fajitaboy.constants;
+
+public class BitmaskConstants {
+	
+	//sound stuff. Mostly masks
+	public static final int AUDIO_FREQ_MASK = 0x38;
+	public static final int AUDIO_UPPER_BYTE = 5;
+	
+}
