c899d037c6cb2ec88029b977544b0dd8ed2f196c
Adam Hulin <adam.hulin@gmail.com>
2009-03-18 21:25:26 +0000

    DebugMemory följer checkstyle

--- src/fajitaboy/DebugMemory.java
+++ src/fajitaboy/DebugMemory.java
@@ -1,10 +1,9 @@
 package fajitaboy;
 
 /**
- * This class is for debugging purposes only. So that the memory
- * returns zero instead of null pointer.
+ * This class is for debugging purposes only. So that the memory returns zero
+ * instead of null pointer.
  * @author Adam Hulin, Johan Gustafsson
- *
  */
 public class DebugMemory implements MemoryInterface {
 
@@ -12,35 +11,36 @@ public class DebugMemory implements MemoryInterface {
      * {@inheritDoc}
      */
     public final int read(final int address) {
-            return 0;
+        return 0;
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public final int forceRead(final int address) {
-        return 	this.read(address);
+        return this.read(address);
     }
 
     /**
      * {@inheritDoc}
      */
     public final void write(final int address, final int data) {
-        //This one does nothing. Data should always be zero.
+        // This one does nothing. Data should always be zero.
     }
-    
+
     /**
      * {@inheritDoc}
      */
     public final void forceWrite(final int address, final int data) {
-    	//same as write();
+        // same as write(), thus doing nothing;
     }
+
     /**
      * {@inheritDoc}
      */
-	public void reset() {
-		//Do nothing since we don't have any data structures
-		
-	}
+    public void reset() {
+        // Do nothing since we don't have any data structures
+
+    }
 
 }
