c0df04593dffa929d28ec56c9188d477e9960e1b
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2009-03-22 15:59:40 +0000

    added constants for sprites. also, number of sprites is 192, not 256.

--- src/fajitaboy/constants/LCDConstants.java
+++ src/fajitaboy/constants/LCDConstants.java
@@ -3,7 +3,9 @@ package fajitaboy.constants;
 public final class LCDConstants {
 	public static final int GB_LCD_W = 160;
 	public static final int GB_LCD_H = 144;
-	public static final int GB_SPRITES = 256;
+	public static final int GB_SPRITES = 192; //borde väl vara 192?
+    public static final int GB_SPRITE_ATTRIBUTES = 40;
+    
 	public static final int GB_TILES = 192;
 	
 	public static final int GB_MAP_W = 32;
