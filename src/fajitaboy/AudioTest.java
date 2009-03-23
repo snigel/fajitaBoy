2546d6c8f2ab665f8932ca70d41a92ffc5e3f67a
Johan Gustafsson <johangu84@gmail.com>
2009-03-23 16:47:20 +0000

    audiotest

--- src/fajitaboy/AudioTest.java
+++ src/fajitaboy/AudioTest.java
@@ -1,4 +1,5 @@
 package fajitaboy;
+
 /**
  * We will test how audio works in java here
  *
@@ -14,28 +15,35 @@ public class AudioTest {
         double frequency = 1000.0;
         double RAD = 2.0 * Math.PI;
         try {
-        	AudioFormat af = new AudioFormat((float)sampleRate,8,1,true,true);
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class,af);
-            SourceDataLine source = (SourceDataLine)AudioSystem.getLine(info);
+            AudioFormat af = new AudioFormat((float) sampleRate, 8, 1, true,
+                    true);
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, af);
+            SourceDataLine source = (SourceDataLine) AudioSystem.getLine(info);
             source.open(af);
             source.start();
             byte[] buf = new byte[sampleRate * seconds];
-            
-            for (int i=0; i<buf.length; i++) {
-                buf[i] = (byte)(Math.sin(RAD*frequency/sampleRate*i)*127.0);
-                //  System.out.println(buf[i]);
+            while (true) {
+                for (int i = 0; i < buf.length; i++) {
+                    buf[i] = (byte) (Math.sin(RAD * frequency / sampleRate * i) * 127.0);
+                    // System.out.println(buf[i]);
+                    /*
+                    long t0,t1;
+                    t0=System.currentTimeMillis();
+                    do{
+                        t1=System.currentTimeMillis();
+                    }
+                    while (t1-t0<100);
+                    */
+
+                }
+
+                source.write(buf, 0, buf.length);
+               
             }
-            
-            source.write(buf,0,buf.length);
-            source.drain();
-            source.stop();
-            source.close();
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
-        
+
         System.exit(0);
     }
 }
-
