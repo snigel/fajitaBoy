e09e31ba43dc5eeef19d2ba336b0aec45aa32737
Adam Hulin <adam.hulin@gmail.com>
2009-03-26 11:36:51 +0000

    Uppdaterade konstanter.

--- src/fajitaboy/constants/BitmaskConstants.java
+++ src/fajitaboy/constants/BitmaskConstants.java
@@ -10,4 +10,7 @@ public class BitmaskConstants {
 	public static final int KEYPAD_BUTTON_MASK = 0x20;
 	public static final int KEYPAD_STATUS_MASK = 0x30;
 	
+	public static final int LOWER_NIBBLE_MASK = 0x0F;
+	public static final int UPPER_NIBBLE_MASK = 0xF0;
+	
 }
