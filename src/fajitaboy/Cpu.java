955fea87f4a2bbd9bed06bd1ef4a4888a0af6ff4
Tobias Svensson <svetob@student.chalmers.se>
2009-04-03 15:05:00 +0000

    Assert's som kontrollerar CPU'ns register. De kommer gnälla om något blir fel.

--- src/fajitaboy/Cpu.java
+++ src/fajitaboy/Cpu.java
@@ -37,7 +37,7 @@ public final class Cpu {
     private boolean ime;
 
     // 8bit registers:
-    /**
+    /**Det gör den faktiskt nu, grejen är att det kommer ändå att komma negativa värden till
      * The H register.
      */
     private int h;
@@ -152,6 +152,19 @@ public final class Cpu {
 
         // Handle interrupts
         handleInterrupts();
+        
+        // Validate CPU registers
+        assert ( a >= 0 && a < 0x100 ) : "CPU register A is out of range: " + a;
+        assert ( b >= 0 && b < 0x100 ) : "CPU register B is out of range: " + b;
+        assert ( c >= 0 && c < 0x100 ) : "CPU register C is out of range: " + c;
+        assert ( d >= 0 && d < 0x100 ) : "CPU register D is out of range: " + d;
+        assert ( e >= 0 && e < 0x100 ) : "CPU register E is out of range: " + e;
+        assert ( h >= 0 && h < 0x100 ) : "CPU register H is out of range: " + h;
+        assert ( l >= 0 && l < 0x100 ) : "CPU register L is out of range: " + l;
+        assert ( cc >= 0 && cc < 0x100 ) : "CPU register CC is out of range: " + cc;
+        assert ( sp >= 0 && sp < 0x10000 ) : "CPU stack pointer SP is out of range: " + sp;
+        assert ( pc >= 0 && pc < 0x10000 ) : "CPU program counter PC is out of range: " + pc;
+        
         return cycleTime;
     }
     
@@ -2382,3 +2395,4 @@ public final class Cpu {
         return executeInterrupt;
     }
 }
+
