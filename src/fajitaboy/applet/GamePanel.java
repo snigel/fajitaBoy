26ef2bb1ab2687837211fc154310fe54f96b08c7
Peter Olsson <olpete@student.chalmers.se>
2009-05-18 16:58:49 +0000

    Målar bara det som har ändrats sedan föregående uppritning. Förbättrar performance avsevärt vid större storlek på appleten.

--- src/fajitaboy/applet/GamePanel.java
+++ src/fajitaboy/applet/GamePanel.java
@@ -43,27 +43,58 @@ public class GamePanel extends JPanel implements VideoReciever {
     	
     	if ( enabled == false )
     		return;
+    	
+      	int minX = Integer.MAX_VALUE;
+    	int minY = Integer.MAX_VALUE; 
+    	int maxX = Integer.MIN_VALUE; 
+    	int maxY = Integer.MIN_VALUE;
     		
     	// refresh pixeldata
         int n = 0;
-        for (int i = 0; i < 144; i++) {
-            for (int j = 0; j < 160; j++) {
-                pixels[n] = data[i][j];
+        for (int y = 0; y < 144; y++) {
+            for (int x = 0; x < 160; x++) {
+                if(pixels[n] != data[y][x]) {
+                	if (x > maxX) {
+                		maxX = x;
+                	}
+                	if (x < minX) {
+                		minX = x;
+                	}
+                	if (y > maxY) {
+                		maxY = y;
+                	}
+                	if (y < minY) {
+                		minY = y;
+                	}
+                	
+                    pixels[n] = data[y][x];
+                }
                 n++;
             }
         }
-
+        
+        if (minX > maxX || minY > maxY) {
+        	return;
+        }
+        maxX++;
+        maxY++;
+        int dx = maxX - minX;
+        int dy = maxY - minY;
         // create image with new pixels
         image = Toolkit.getDefaultToolkit().createImage(
-                new MemoryImageSource(160, 144, colorModel, pixels, 0, 160));
+                new MemoryImageSource(dx, dy, colorModel, pixels, minX + minY*160, 160));
+
+        
         // draw the image
         Graphics g = getGraphics();
         if(g != null) {
-        	g.drawImage(image, 0, 0, 160 * zoom, 144 * zoom, null);
+        	g.drawImage(image,minX*zoom,minY*zoom,dx*zoom,dy*zoom,null);
         }
     }
 
     public void paint(Graphics g) {
+        image = Toolkit.getDefaultToolkit().createImage(
+                new MemoryImageSource(160, 144, colorModel, pixels, 0, 160));    	
         g.drawImage(image, 0, 0, 160 * zoom, 144 * zoom, null);
     }
     
